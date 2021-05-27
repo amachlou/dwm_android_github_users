0e32158b160b502892fee35b2e3ea3d06befa7e5
Jijinet <jihanrabat345@gmail.com>
2021-05-27 01:40:09 +0100

    fixing image

--- app/src/main/java/com/machlou/dwm_java/dwm_android_github_users/model/UsersListViewModel.java
+++ app/src/main/java/com/machlou/dwm_java/dwm_android_github_users/model/UsersListViewModel.java
@@ -1,5 +1,7 @@
 package com.machlou.dwm_java.dwm_android_github_users.model;
 
+
+import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -7,13 +9,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.machlou.dwm_java.dwm_android_github_users.MainActivity;
 import com.machlou.dwm_java.dwm_android_github_users.R;
 
+
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
 
@@ -22,8 +28,10 @@ import de.hdodenhof.circleimageview.CircleImageView;
 public class UsersListViewModel extends ArrayAdapter<GitUser> {
     private int resource;
     public UsersListViewModel(@NonNull Context context, int resource, List<GitUser> data) {
-        super(context, resource,data);this.resource=resource;
+        super(context, resource,data);
+        this.resource=resource;
     }
+
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
@@ -36,14 +44,19 @@ public class UsersListViewModel extends ArrayAdapter<GitUser> {
         TextView textViewScore=listViewItem.findViewById(R.id.textViewScore);
         textViewLogin.setText(getItem(position).login);
         textViewScore.setText(String.valueOf(getItem(position).score));
-        try {
-            URL url=new URL(getItem(position).avatarUrl);
-            Bitmap bitmap= BitmapFactory.decodeStream(url.openStream());
-            imageViewUser.setImageBitmap(bitmap);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-//imageViewUser.setImageBitmap();
+        new Thread(()->{
+            try {
+                URL url=new URL(getItem(position).avatarUrl);
+                Bitmap bitmap= BitmapFactory.decodeStream(url.openStream());
+                ((Activity)getContext()).runOnUiThread(()->{
+                    imageViewUser.setImageBitmap(bitmap);
+                });
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        //imageViewUser.setImageBitmap();
         return listViewItem;
     }
 }
